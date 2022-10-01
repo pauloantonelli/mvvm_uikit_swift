4618fdf93b5dfdfa3496287b838a17455521b4be
pauloantonelli <pauloantonelli@hotmail.com>
2022-10-01 12:29:55 -0300

    Initial Commit

---
+++ MVVM StoryBoard Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM StoryBoard Example
+//
+//  Created by Paulo Antonelli on 01/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
